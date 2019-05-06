044a66a9ec4726bcc0bf0af36d67652d31236849
seekm <seekm@users.noreply.github.com>
2019-05-06 11:58:10 +0200

    Update

--- sikuli/explorer.sikuli/explorer.py
+++ sikuli/explorer.sikuli/explorer.py
@@ -18,4 +18,13 @@ def export_file():
     type(Key.DOWN)
     type(Key.DOWN)
     type(Key.DOWN)
-    type(Key.ENTER)    
+    type(Key.ENTER)
+
+def export_git():
+    click("1557085792485.png")
+    mouseMove(0,100)
+    rightClick(Mouse.at())
+    click("1557086004405.png")
+    sleep(1)
+    type(Key.LEFT, KeyModifier.WIN)
+    click("1557086414955.png")
