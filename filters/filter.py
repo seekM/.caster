01343d84580fdd35192eb9243731c7b9a8b4f518
seekm <seekm@users.noreply.github.com>
2019-06-05 09:23:04 +0200

    Update

--- filters/filter.py
+++ filters/filter.py
@@ -13,15 +13,35 @@ def replace_spec(rule, target, replacement):
         del rule.mapping_actual()[target]
         rule.mapping_actual()[replacement] = action
 
-
 def replace_spec_scenario(mp):
     if mp.time == MergeInf.BOOT:
         if mp.rule1 is not None:
             replace_spec(mp.rule1, "numb <wnKK>", "number <wnKK>")
             replace_spec(mp.rule1, "word number <wn>", "word digit <wn>")
+            replace_spec(mp.rule1, "quotes", "(quotes|zit)")
+            replace_spec(mp.rule1, "thin quotes", "(small quotes |zit klei)")
+            replace_spec(mp.rule1, "pipe (sim | symbol)", "(rohr | pipe symbol)")
+            replace_spec(mp.rule1, "long pipe (sim | symbol)", "(lang rohr | long pipe symbol)")
+            replace_spec(mp.rule1, "comma", "(comma|komm)")
+            replace_spec(mp.rule1, "shift right click", "test")
+            replace_spec(mp.rule1, "curse <direction> [<direction2>] [<nnavi500>] [<dokick>]", "(curse|maus) <direction> [<direction2>] [<nnavi500>] [<dokick>]")
+            replace_spec(mp.rule1, "spark [<nnavi500>] [(<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel)]", "spark [<nnavi500>] [(<capitalization> <spacing> | <capitalization> | <spacing>)]")
+            replace_spec(mp.rule1, "set [<big>] format (<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel)", "set [<big>] format (<capitalization> <spacing> | <capitalization> | <spacing>)")
+            replace_spec(mp.rule1, "(<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel) <textnv> [brunt]", "(<capitalization> <spacing> | <capitalization> | <spacing>) <textnv>")
+   
         replace_spec(mp.rule2, "numb <wnKK>", "number <wnKK>")
         replace_spec(mp.rule2, "word number <wn>", "word digit <wn>")
-
+        replace_spec(mp.rule2, "quotes", "(quotes|zit)")
+        replace_spec(mp.rule2, "thin quotes", "(small quotes |zit klei)")
+        replace_spec(mp.rule2, "pipe (sim | symbol)", "(rohr | pipe symbol)")
+        replace_spec(mp.rule2, "long pipe (sim | symbol)", "(lang rohr | long pipe symbol)")
+        replace_spec(mp.rule2, "comma", "(comma|komm)")
+        replace_spec(mp.rule2, "shift right click", "test")
+        replace_spec(mp.rule2, "curse <direction> [<direction2>] [<nnavi500>] [<dokick>]", "(curse|haus) <direction> [<direction2>] [<nnavi500>] [<dokick>]")
+        replace_spec(mp.rule2, "spark [<nnavi500>] [(<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel)]", "spark [<nnavi500>] [(<capitalization> <spacing> | <capitalization> | <spacing>)]")
+        replace_spec(mp.rule2, "set [<big>] format (<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel)", "set [<big>] format (<capitalization> <spacing> | <capitalization> | <spacing>)")
+        replace_spec(mp.rule2, "(<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel) <textnv> [brunt]", "(<capitalization> <spacing> | <capitalization> | <spacing>) <textnv>")
+ 
 def update_carrot(rule):
     if "carrot" in rule.mapping_actual().keys():
         rule.mapping_actual()["carrot"] = R(Key("caret") + Key("left") + Key("delete"))
